6b98154b3ff38d179cb92dd609f41f2fdfec29dd
Msvenda <sv.marin26@gmail.com>
2020-12-11 10:41:18 +0100

    started work on spy command

--- BWChatRanges/src/com/brokenworldrp/chatranges/commands/SpyCommand.java
+++ BWChatRanges/src/com/brokenworldrp/chatranges/commands/SpyCommand.java
@@ -1,5 +1,22 @@
 package com.brokenworldrp.chatranges.commands;
 
-public class SpyCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.brokenworldrp.chatranges.utils.MessageUtils;
+
+public class SpyCommand implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(!(sender instanceof Player))	{
+			MessageUtils.sendPlayersOnlyMessage(sender);
+			return true;
+		}
+		Player player = (Player) sender;
+		return false;
+	}
 
 }
